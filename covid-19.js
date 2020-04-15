6f1777935781b56d78873f131d83094718930358
salmanAltaf101 <sllujaan44@gmail.com>
2020-04-15 17:37:31 +0500

    making containers....

--- covid-19.js
+++ covid-19.js
@@ -4,7 +4,9 @@ import {covid_data} from './fetch_api/covid-19_fetch_api.js'
 
 
 var body = document.getElementsByTagName("body")[0]
-
+export var confirmed = document.getElementsByClassName("conformed")[0].getElementsByClassName("value")[0]
+export var recovered = document.getElementsByClassName("recovered")[0].getElementsByClassName("value")[0]
+export var deaths = document.getElementsByClassName("deaths")[0].getElementsByClassName("value")[0]
 
 
 var country = document.getElementById("countries-covid-19")
@@ -12,8 +14,14 @@ console.log(country.value)
 
 country.addEventListener('change', (e) => {
     console.log(e.target.value)
+    
 })
 
+
+
+var showData = (country) => {
+    
+}
 
 
 
--- fetch_api/covid-19_fetch_api.js
+++ fetch_api/covid-19_fetch_api.js
@@ -1,13 +1,13 @@
 
 
-import {showNetworkError} from '../covid-19.js'
+import {showNetworkError, confirmed, recovered, deaths} from '../covid-19.js'
 
 
 var url = 'https://covid19.mathdro.id/api'
 
 
-export var covid_data = async () => {
-    
+export var covid_data = async (country, typeOfData) => {
+
     var res = await fetch(url)
 
     if(!res.ok) throw Error(res.status + " " + res.statusText)
@@ -15,13 +15,17 @@ export var covid_data = async () => {
     var data = await res.json()
 
     return data;
+
+    
 }
 
 
 
 covid_data()
 .then(data => {
-    console.log(data.confirmed)
+    console.log(data)
+    setAll(data)
+    console.log(getConfirmed(data, 'France'))
 })
 .catch(err => {
     console.error(err)
@@ -31,14 +35,45 @@ covid_data()
 
 
 
-
+//getter functions---------------------------------------
 var getConfirmed = (data, country) => {
+
+    if(country) {
+        var countryRegion = data.find(index => {
+            return index.countryRegion === country
+        })
+
+        if(countryRegion) return countryRegion.confirmed
+        
+        return
+    }
+
     return data.confirmed.value
 }
 
-
-
-var setConfirmed = () => {
-    
+var getRecovered = (data, country) => {
+    return data.recovered.value
 }
+var getDeaths = (data, country) => {
+    return data.deaths.value
+}
+//--------------------------------------------------
 
+
+//setter functions---------------------------------------
+var setConfirmed = (value) => {
+    confirmed.innerText = value
+}
+var setRecovered = (value) => {
+    recovered.innerText = value
+}
+var setDeaths = (value) => {
+    deaths.innerText = value
+}
+//--------------------------------------------------
+
+var setAll = (data) => {
+    setConfirmed(getConfirmed(data))
+    setRecovered(getRecovered(data))
+    setDeaths(getDeaths(data))
+}
